903d1369bf80f5bc3aed6d9df75a19f9549a93d6
zbianca <bianca.zanardi@gmail.com>
2018-05-13 23:30:12 +0200

    Refactor card and add alt text to images

--- js/main.js
+++ js/main.js
@@ -136,30 +136,16 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  * Create restaurant HTML.
  */
 createRestaurantHTML = (restaurant) => {
+
+  cardHtml = `<a href="${DBHelper.urlForRestaurant(restaurant)}">
+              <img class="restaurant-img" src="${DBHelper.imageUrlForRestaurant(restaurant)}" alt="${restaurant.photo_description}">
+              <h1>${restaurant.name}</h1>
+              <p>${restaurant.neighborhood}</p>
+              <p>${restaurant.address}</p>
+              <a class="restaurants-list-cta" href="${DBHelper.urlForRestaurant(restaurant)}">View Details</a></a>`
+
   const li = document.createElement('li');
-
-  const image = document.createElement('img');
-  image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = ''
-  li.append(image);
-
-  const name = document.createElement('h1');
-  name.innerHTML = restaurant.name;
-  li.append(name);
-
-  const neighborhood = document.createElement('p');
-  neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
-
-  const address = document.createElement('p');
-  address.innerHTML = restaurant.address;
-  li.append(address);
-
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.innerHTML = cardHtml;
 
   return li
 }
